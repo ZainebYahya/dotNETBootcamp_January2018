c2628a1e9b7f96a306e445685c361ddcf1223b00
kamel-rush <kamel@grandcircus.co>
2018-01-26 13:14:02 -0500

    new code

---
+++ TestMethods2018/TestMethods2018/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace TestMethods2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            //int X, Y;
+
+            //X = ReadUserInput("Please enter X");
+
+            //Y = ReadUserInput("Please enter Y");
+
+            //Console.WriteLine("The largest number is:" + FindMax(X,Y));
+
+
+            ReadValidOption();
+        }
+
+
+        // define a method that reads user input and returns that input
+        public static int ReadUserInput(string UserPrompt)
+        {
+            Console.WriteLine(UserPrompt);
+            int Input;
+            string UserInput = Console.ReadLine();
+            while (!Regex.Match(UserInput, @"^\d{1,10}$").Success)
+            {
+                Console.WriteLine("Please enter a valid number!");
+                UserInput = Console.ReadLine();
+            }
+            // after the while loop, your string will be valid! 
+            Input = int.Parse(UserInput);
+            return Input;
+            //while (!int.TryParse(Console.ReadLine(),out Input))
+            //{
+            //    Console.WriteLine("Please enter a valid number!");
+            //}
+
+            // int Input = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public static int ReadUserInput()
+        {
+            Console.WriteLine("Give me some input!");
+            int Input = Convert.ToInt32(Console.ReadLine());
+            return Input;
+        }
+
+        // define a method to find the largest of two integers 
+        public static int FindMax(int x, int y)
+        {
+            //if (x > y)
+            //    return x;
+            //else
+            //    return y;
+
+            return (x > y) ? x : y; // condition ? true part : false part 
+        }
+
+
+        public static string ReadValidOption()
+        {
+            string Input = Console.ReadLine();
+
+            while (!Regex.Match(Input, @"^(yes|y|no|n)$").Success)
+            {
+                Console.WriteLine("Please enter only yes|y|no|n");
+                Input = Console.ReadLine();
+            }
+
+            return Input;
+
+        }
+    }
+}
---
+++ TestStrings2018/TestStrings2018/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace TestStrings2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+           
+            // string str = "Hello World!";// reference type 
+
+            // string str1 = new string('*', 50);
+
+
+            //// Console.WriteLine(str1);
+
+            // Console.WriteLine(str[5]);
+            // Console.WriteLine(str[str.Length-1]);
+
+            // for (int i = 0; i < str.Length; i++) // loop will go from 0 to length-1
+            // {
+            //     Console.WriteLine(str[i]);
+            // }
+
+
+            //string UserChoice = Console.ReadLine();
+
+            //if (string.Compare(UserChoice, "y", true)==0)
+            //{
+            //    Console.WriteLine("you entered y");
+
+            //}
+
+            // Console.WriteLine(string.Compare("Hello", "Apple", true));
+            // 0 if they are exactly the same 
+            // -1 if the first string shows before the second one in the alphabets order
+            // 1 if the first string shows after the second one in the alphabets order
+
+           // string UserInput = Console.ReadLine();
+
+            //Console.WriteLine(UserInput.IndexOf("r"));
+
+           // Console.WriteLine(UserInput.LastIndexOf("r"));
+
+
+           // Console.WriteLine();
+
+
+
+            //StringBuilder sb1 = new StringBuilder("Hello World");
+
+            //sb1.Append(" from Gc");
+
+            //Console.WriteLine(sb1);
+
+            
+           
+            
+
+          
+        }
+    }
+}
