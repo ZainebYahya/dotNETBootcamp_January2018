ea319fa7698083b59eadb6b819e01abcd70671b0
kamel-rush <kamel@grandcircus.co>
2018-02-02 14:00:29 -0500

    OOP code

---
+++ TestOOP2018/TestOOP2018/Employee.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestOOP2018
+{
+    class Employee
+    {
+        // Fields 
+        #region Fields
+        private string name;
+        private string address;
+        private string ssn;
+        private float salary;
+        private string phoneno;
+        #endregion
+
+        #region properties
+        public string Name
+        {
+            set { name = value; } // used to change the value of the field 
+            get { return name; } // used to return the value of the field 
+
+        }
+
+        public string Address
+        {
+            set { address = value; } // used to change the value of the field 
+            get { return address; } // used to return the value of the field 
+
+        }
+
+        public string SSN
+        {
+            set {
+                if (!Regex.Match(value, @"^\d{9}$").Success)
+                    throw new FormatException("Wrong SSN format");
+                else
+                ssn = value;
+
+
+            } // used to change the value of the field 
+            get { return ssn; } // used to return the value of the field 
+
+        }
+
+        public string Phoneno
+        {
+            set { phoneno = value; } // used to change the value of the field 
+            get { return phoneno; } // used to return the value of the field 
+
+        }
+
+        public float Salary
+        {
+            set { salary = value;
+               // value = salary;  /// BUG!!!!!!!
+            } // used to change the value of the field 
+            get { return salary; } // used to return the value of the field 
+
+        }
+        #endregion
+
+        #region Constructors
+        public Employee(string employeeName, string employeeAddress)
+        {
+            Name = employeeName;
+            Address = employeeAddress; 
+        }
+
+        public Employee():this("no provided!", "1570")
+        {
+            //Name = "no provided!";
+            //Address = "1570";
+        }
+        #endregion
+
+
+        // Methods
+
+        // print the employee's info
+        public void PrintInfo()
+        {
+            Console.WriteLine(new string('*',20));
+            Console.WriteLine(Address);
+            Console.WriteLine(Name);
+            Console.WriteLine(new string('*', 20));
+        }
+
+
+        // give a raise to an employee 
+
+        public void GiveRaise(float Percentage)
+        {
+            float Raise;
+            Raise = (Percentage / 100) + 1;
+            Salary *= Raise; 
+
+        }
+
+
+        void MyMethod(Func<int, int> objFunc)
+        {
+
+
+
+
+        }
+    }
+}
---
+++ TestOOP2018/TestOOP2018/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOP2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type variablename = new type();            ()=> constructors 
+
+            Employee Jacob = new Employee("Jacob", "1570 woodward");
+
+            Employee John = new Employee();
+
+            Jacob.Name = "Jacob Snover";// set 
+
+            Jacob.Address = "1570 Woodward Ave., Detroit, MI, 48202"; // set 
+
+            Jacob.PrintInfo();
+
+            Jacob.Salary = 2000;
+
+            Jacob.GiveRaise(3);
+
+            Console.WriteLine(Jacob.Salary);
+
+
+            // using arrays or other DS to store an manipulate objects! 
+
+
+            Employee[] EmpList = new Employee[10];
+
+            for (int i = 0; i < EmpList.Length; i++)
+            {
+                EmpList[i] = new Employee();
+
+                Console.WriteLine("Please enter the name of the employee");
+                EmpList[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Please enter the address of the employee");
+                EmpList[i].Address = Console.ReadLine();
+
+                Console.WriteLine("Please enter the SSN of the employee");
+                EmpList[i].SSN = Console.ReadLine();
+
+                Console.WriteLine("Please enter the Salary of the employee");
+                EmpList[i].Salary = float.Parse(Console.ReadLine());
+
+            }
+
+            // write a code that prints the names of all employees with a salary larger than 2000
+
+            for (int i = 0; i < EmpList.Length; i++)
+            {
+                if (EmpList[i].Salary > 2000)
+                {
+                    Console.WriteLine(EmpList[i].Name);
+                }
+            }
+
+            foreach (Employee EMP in EmpList)
+            {
+                if (EMP.Salary > 2000)
+                {
+                    Console.WriteLine(EMP.Name);
+                }
+            }
+
+        }
+    }
+}
