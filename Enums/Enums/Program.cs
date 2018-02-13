7449b6fe777bfecbb28cd2e719d4bfa4290a6261
kamel-rush <kamel@grandcircus.co>
2018-02-13 14:25:10 -0500

    new code

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float? salary = null;
+
+            // int @int = 4; 
+           
+            Console.WriteLine(Sum(1,1,2));
+
+            Console.WriteLine(Sum(1, 1, 2,4,56,6,7,8,89,9,90,09,0));
+        }
+
+        public static int Sum(params int[] ar)
+        {
+            int sum = 0;
+
+            foreach (int element in ar)
+                sum += element;
+
+            return sum; 
+
+        }
+    }
+}
---
+++ TestThreads/TestThreads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading; 
+
+namespace TestThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(Play));
+                t.Start(i);// will start a new thread
+                
+              //  Play(i);
+            }
+
+        }
+
+        public static void Play(object x)
+        {
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
